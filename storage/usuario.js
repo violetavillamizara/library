ca03a5be75bda0bb3b11f67fec6fe557a3584231
violetavillamizara <villamizaravioleta@gmail.com>
2023-10-03 13:33:22 -0500

    feat: :sparkles: functions db

--- storage/usuario.js
+++ storage/usuario.js
@@ -1,18 +1,17 @@
-import env from"../config.js";
+import env from "../config.js";
 
 const uri=`${env.ssl+env.hostName}:${env.port}`;
 const config={method:undefined,headers:{"Content-Type":"application/json"}};
 
-
 export const getAll= async()=>{
     config.method="GET";
-    let res= await(await fetch(`${uri}/libro`, config)).json();
+    let res= await(await fetch(`${uri}/usuario`, config)).json();
 }
 
 export const post= async(obj)=>{
     config.method="POST";
     config.body=JSON.stringify(obj);
-    let res= await(await fetch(`${uri}/libro`, config)).json();
+    let res= await(await fetch(`${uri}/usuario`, config)).json();
     return res;
 }
 
@@ -20,14 +19,14 @@ export const deleteOne = async(id)=>{
     if(typeof id!=='number')return {status:400, message:"`El dato '${id}' no cumple`"};
     config.method="DELETE";
     config.body=JSON.stringify(obj);
-    let res= await(await fetch(`${uri}/libro/${id}`, config)).json();
+    let res= await(await fetch(`${uri}/usuario/${id}`, config)).json();
     return res;
 
 }
 
 export const putOne= async(obj)=>{
     const {id}=obj;
-    if(Object.constructor.name !=='Object')return {status:400, message:"`El dato '${id}' no cumple`"};
+    if(typeof id!=='string')return {status:400, message:"`El dato '${id}' no cumple`"};
 }
 
 deletemany
